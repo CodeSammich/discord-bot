8a465f5df0a0be40d2dfa347978163b50e6d6dfd
Samuel Zhang <samuelzhang3@gmail.com>
2019-04-02 14:31:43 -0400

    automate secret_key

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@ sudo add-apt-repository universe -y;
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y;
 sudo apt update;
 sudo apt install python3-pip espeak ffmpeg libopus0 opus-tools git -y;
-cd ~
-git clone https://github.com/CodeSammich/discord-bot
-cd ~/discord-bot
 pip3 install -r requirements.txt
 touch secret_key.txt
 echo "$0" >> secret_key.txt
