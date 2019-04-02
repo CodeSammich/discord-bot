3eaed2ff3874f8bf5297037be1f4c353fc35533d
Samuel Zhang <CodeSammich@users.noreply.github.com>
2019-04-02 17:33:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+echo "Adding necessary respositories
 sudo add-apt-repository universe -y;
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y;
 sudo apt update;
+echo "Installing all necessary packages"
 sudo apt install python3 python3-pip espeak ffmpeg libopus0 opus-tools git -y;
+echo "Preventing Pip from being Upgraded"
+sudo apt-mark hold python-pip
+echo "Upgrading all other packages"
 sudo apt upgrade -y;
+echo "Installing necessary Python3 packages"
 pip3 install -r requirements.txt;
+echo "Creating secret_key.txt necessary for Discord server authentication"
 touch secret_key.txt;
 echo "$0" >> secret_key.txt;
