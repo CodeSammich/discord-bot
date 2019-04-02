04583e9731c3e728d7a98c85fb09394183e31b39
Samuel Zhang <samuelzhang3@gmail.com>
2019-04-02 14:42:36 -0400

    automate python3.6 installation, only on 18.04

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sudo add-apt-repository universe -y;
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y;
 sudo apt update;
 sudo apt install python3 python3-pip espeak ffmpeg libopus0 opus-tools git -y;
+sudo apt upgrade;
 pip3 install -r requirements.txt
 touch secret_key.txt
 echo "$0" >> secret_key.txt
