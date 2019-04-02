ae0e7e11e642916f19a16f08d363cf0b9ff5558d
Samuel Zhang <CodeSammich@users.noreply.github.com>
2019-04-02 17:24:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo add-apt-repository universe -y;
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y;
 sudo apt update;
 sudo apt install python3 python3-pip espeak ffmpeg libopus0 opus-tools git -y;
-sudo apt upgrade;
+sudo apt upgrade -y;
 pip3 install -r requirements.txt
 touch secret_key.txt
 echo "$0" >> secret_key.txt
